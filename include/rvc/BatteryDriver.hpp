bbb5238b978622d2eb549a1fd6559cdee9282b9b
YoonBoorue <boorue03@gmail.com>
2026-05-04 12:59:13 +0900

    feat: implement battery module from skeleton

--- include/rvc/BatteryDriver.hpp
+++ include/rvc/BatteryDriver.hpp
@@ -1,20 +1,38 @@
 #pragma once
 
-namespace rvc {
+namespace rvc
+{
 
-class BatteryDriver {
-private:
-    int LV;
-    bool status;
+    class BatteryDriver
+    {
+    private:
+        int LV;
+        bool status;
+        bool charging;
+        bool powered;
 
-public:
-    BatteryDriver();
-    void initialize();
-    void turnOffBattery();
-    void declineLV();
-    void inclineLV();
-    void startCharging();
-    void stopCharging();
-};
+    public:
+        static constexpr int MaxLevel = 100;
+        static constexpr int MinLevel = 0;
+        static constexpr int LowBatteryThreshold = 10;
+        static constexpr int ChargeStep = 10;
+        static constexpr int DischargeStep = 1;
+
+        BatteryDriver();
+        void initialize();
+        void turnOffBattery();
+        void declineLV();
+        void inclineLV();
+        void startCharging();
+        void stopCharging();
+
+        int level() const;
+        bool isFull() const;
+        bool isLow() const;
+        bool isCharging() const;
+        bool isPowered() const;
+
+        void setLevel(int level);
+    };
 
 }
--- src/rvc/BatteryDriver.cpp
+++ src/rvc/BatteryDriver.cpp
@@ -1,31 +1,98 @@
 #include "rvc/BatteryDriver.hpp"
 
-namespace rvc {
+#include <algorithm>
 
-BatteryDriver::BatteryDriver() : LV(100), status(false) {}
+namespace rvc
+{
 
-void BatteryDriver::initialize() {
-    // TODO
-}
+    BatteryDriver::BatteryDriver()
+        : LV(MaxLevel), status(true), charging(false), powered(false) {}
 
-void BatteryDriver::turnOffBattery() {
-    // TODO
-}
+    void BatteryDriver::initialize()
+    {
+        powered = true;
+        status = (LV >= MaxLevel);
+    }
 
-void BatteryDriver::declineLV() {
-    // TODO
-}
+    void BatteryDriver::turnOffBattery()
+    {
+        powered = false;
+        charging = false;
+    }
 
-void BatteryDriver::inclineLV() {
-    // TODO
-}
+    void BatteryDriver::declineLV()
+    {
+        if (LV > MinLevel)
+        {
+            LV = std::max(MinLevel, LV - DischargeStep);
+        }
+        status = (LV >= MaxLevel);
+    }
 
-void BatteryDriver::startCharging() {
-    // TODO
-}
+    void BatteryDriver::inclineLV()
+    {
+        if (!charging)
+        {
+            return;
+        }
 
-void BatteryDriver::stopCharging() {
-    // TODO
-}
+        if (LV < MaxLevel)
+        {
+            LV = std::min(MaxLevel, LV + ChargeStep);
+        }
+
+        status = (LV >= MaxLevel);
+        if (status)
+        {
+            charging = false;
+        }
+    }
+
+    void BatteryDriver::startCharging()
+    {
+        charging = (LV < MaxLevel);
+        status = (LV >= MaxLevel);
+    }
+
+    void BatteryDriver::stopCharging()
+    {
+        charging = false;
+        status = (LV >= MaxLevel);
+    }
+
+    int BatteryDriver::level() const
+    {
+        return LV;
+    }
+
+    bool BatteryDriver::isFull() const
+    {
+        return status;
+    }
+
+    bool BatteryDriver::isLow() const
+    {
+        return LV <= LowBatteryThreshold;
+    }
+
+    bool BatteryDriver::isCharging() const
+    {
+        return charging;
+    }
+
+    bool BatteryDriver::isPowered() const
+    {
+        return powered;
+    }
+
+    void BatteryDriver::setLevel(int level)
+    {
+        LV = std::clamp(level, MinLevel, MaxLevel);
+        status = (LV >= MaxLevel);
+        if (status)
+        {
+            charging = false;
+        }
+    }
 
 }
